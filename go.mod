c27e77a79e7ba8a389b4fbac9e6ba6e83e7d4d42
dvaumoron <dvaumoron@gmail.com>
2023-02-03 15:28:02 +0100

    refactor with grpcserver

--- go.mod
+++ go.mod
@@ -3,17 +3,18 @@ module github.com/dvaumoron/puzzlemarkdownserver
 go 1.19
 
 require (
+	github.com/dvaumoron/puzzlegrpcserver v1.0.1
 	github.com/dvaumoron/puzzlemarkdownservice v1.0.1
-	github.com/joho/godotenv v1.4.0
 	github.com/yuin/goldmark v1.5.3
-	google.golang.org/grpc v1.52.3
 )
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	golang.org/x/net v0.4.0 // indirect
 	golang.org/x/sys v0.3.0 // indirect
 	golang.org/x/text v0.5.0 // indirect
 	google.golang.org/genproto v0.0.0-20221118155620-16455021b5e6 // indirect
+	google.golang.org/grpc v1.52.3 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 )
--- server.go
+++ server.go
@@ -18,30 +18,13 @@
 package main
 
 import (
-	"log"
-	"net"
-	"os"
-
+	grpcserver "github.com/dvaumoron/puzzlegrpcserver"
 	"github.com/dvaumoron/puzzlemarkdownserver/markdownserver"
 	pb "github.com/dvaumoron/puzzlemarkdownservice"
-	"github.com/joho/godotenv"
-	"google.golang.org/grpc"
 )
 
 func main() {
-	if godotenv.Overload() == nil {
-		log.Println("Loaded .env file")
-	}
-
-	lis, err := net.Listen("tcp", ":"+os.Getenv("SERVICE_PORT"))
-	if err != nil {
-		log.Fatal("Failed to listen :", err)
-	}
-
-	s := grpc.NewServer()
+	s := grpcserver.New()
 	pb.RegisterMarkdownServer(s, markdownserver.New())
-	log.Println("Listening at", lis.Addr())
-	if err := s.Serve(lis); err != nil {
-		log.Fatal("Failed to serve :", err)
-	}
+	s.Start()
 }
